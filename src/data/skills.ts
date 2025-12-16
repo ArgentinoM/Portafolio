814a02ca98ecd19f9c104a2d7ab501e642427bbc
Argentino Monroy <argenmv06@gmail.com>
2025-12-16 04:08:33 -0600

    first commit

---
+++ src/data/skills.ts
@@ -0,0 +1,77 @@
+interface Skill{
+    imgUrl: string;
+    name: string;
+}
+
+
+export const Skills: Skill[] = [
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
+        name: 'Html'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
+        name: 'Css'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+        name: 'JavaScript'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg',
+        name: 'typescript'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg',
+        name: 'Sass'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
+        name: 'Tailwind'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg',
+        name: 'Angular'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/astro/astro-original.svg',
+        name: 'Astro'
+    },
+    // {
+    //     imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    //     name: 'React'
+    // },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg',
+        name: 'rxjs'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg',
+        name: 'vitejs'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitest/vitest-original.svg',
+        name: 'vitest'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original.svg',
+        name: 'Laravel'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg',
+        name: 'Mysql'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mariadb/mariadb-original.svg',
+        name: 'mariadb'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg',
+        name: 'Postman'
+    },
+    {
+        imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg',
+        name: 'git'
+    },
+
+]
